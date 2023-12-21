629d960be2fa77124a917184f23a0fe3a489a588
beerose <alexsandra.sikora@gmail.com>
2023-12-21 17:32:34 +0100

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
+          <div className="z-10 md:max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-4xl font-bold text-center">
+                Welcome to <span className="text-blue-500">M</span>
+                <span className="text-red-500">a</span>
+                <span className="text-yellow-500">t</span>
+                <span className="text-blue-500">h</span>
+                <span className="text-green-500">P</span>
+                <span className="text-red-500">u</span>
+                <span className="text-yellow-500">z</span>
+                <span className="text-blue-500">z</span>
+                <span className="text-green-500">l</span>
+                <span className="text-red-500">e</span>
+                <span className="text-yellow-500">s</span>
+              </h1>
+              <p className="text-center mt-2">
+                A multiplayer math puzzle game to play and outsmart your
+                friends.
+              </p>
+              {children}
+            </div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
